ee41b11c55dd0de859ec43a0961cf93189d87f72
jpmitri <60190789+jpmitri@users.noreply.github.com>
2021-06-26 17:18:28 +0300

    SignalR init

---
+++ WebAPI/Hubs/OutletHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using BLC;
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebAPI.Hubs
+{
+    public class OutletHub : Hub
+    {
+        public async void SubscribeToOutlet(Outlet[] ChangedOutlets)
+        {
+            await Clients.All.SendCoreAsync("FetchData", ChangedOutlets);
+        }
+    }
+}
