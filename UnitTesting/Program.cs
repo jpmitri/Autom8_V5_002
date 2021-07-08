17c16c40e7f619f93d52f5d12777f7c95551c862
jpmitri <jpmitri@outlook.com>
2021-07-08 12:58:36 +0300

    10

--- UnitTesting/Program.cs
+++ UnitTesting/Program.cs
@@ -38,6 +38,7 @@ namespace UnitTesting
                 Console.WriteLine("004 --> Create New User");
                 Console.WriteLine("005 --> Import From Old With Auto Init");
                 Console.WriteLine("006 --> Import From Old With Default Floor And Room");
+                Console.WriteLine("007 --> Import From Old With Default Floor And Room With Out User Generation");
                 Console.WriteLine("009 --> Init Test");
                 Console.WriteLine("010 --> Test Digital");
                 Console.WriteLine("011 --> Test Dimmer");
@@ -429,22 +430,20 @@ namespace UnitTesting
                                     reader.Close();
                                     connection.Close();
                                 }
-
-
-                                #region Create First User
-                                User user = new();
-                                user.USER_ID = -1;
-                                user.OWNER_ID = 1;
-                                Console.WriteLine("Enter Username");
-                                user.USERNAME = Console.ReadLine();
-                                Console.WriteLine("Enter Password");
-                                user.PASSWORD = Console.ReadLine();
-                                user.ENTRY_DATE = oTools.GetDateString(DateTime.Today);
-                                user.IS_ACTIVE = true;
-                                user.USER_TYPE_CODE = "002";
-                                oBLC.Edit_User(user);
-                                #endregion
                             }
+                            #endregion
+                            #region Create First User
+                            User user = new();
+                            user.USER_ID = -1;
+                            user.OWNER_ID = 1;
+                            Console.WriteLine("Enter Username");
+                            user.USERNAME = Console.ReadLine();
+                            Console.WriteLine("Enter Password");
+                            user.PASSWORD = Console.ReadLine();
+                            user.ENTRY_DATE = oTools.GetDateString(DateTime.Today);
+                            user.IS_ACTIVE = true;
+                            user.USER_TYPE_CODE = "002";
+                            oBLC.Edit_User(user);
                             #endregion
                         }
                         break;
@@ -577,25 +576,156 @@ namespace UnitTesting
                                     reader.Close();
                                     connection.Close();
                                 }
+                            }
+                            #endregion
+                            #region Create First User
+                            User user = new();
+                            user.USER_ID = -1;
+                            user.OWNER_ID = 1;
+                            Console.WriteLine("Enter Username");
+                            user.USERNAME = Console.ReadLine();
+                            Console.WriteLine("Enter Password");
+                            user.PASSWORD = Console.ReadLine();
+                            user.ENTRY_DATE = oTools.GetDateString(DateTime.Today);
+                            user.IS_ACTIVE = true;
+                            user.USER_TYPE_CODE = "002";
+                            oBLC.Edit_User(user);
+                            #endregion
+                        }
+                        break;
+                    case "007":
+                        {
+                            #region Add Default Floor And Room
+                            Floor floor = new();
+                            floor.FLOOR_ID = -1;
+                            floor.NAME = "Default Floor";
+                            oBLC.Edit_Floor(floor);
+                            Room room = new();
+                            room.ROOM_ID = -1;
+                            room.FLOOR_ID = floor.FLOOR_ID;
+                            room.NAME = "Default Room";
+                            oBLC.Edit_Room(room);
+                            #endregion
+                            #region Import
+                            string OldDB = @"" + ConfigurationManager.AppSettings["CONN_STR_OLD"];
+                            using(SqlConnection connection = new(OldDB))
+                            {
+                                Plc plc = new();
+                                using(SqlCommand command = new())
+                                {
+                                    command.CommandText = "SELECT AMSID, PORT FROM TBL_BOARD";
+                                    command.Connection = connection;
+                                    connection.Open();
+                                    SqlDataReader reader = command.ExecuteReader();
+                                    if(reader.HasRows)
+                                    {
+                                        while(reader.Read())
+                                        {
+                                            plc.PLC_ID = -1;
+                                            plc.PORT = reader["PORT"].ToString();
+                                            plc.LOCATION = reader["AMSID"].ToString();
+                                            plc.CURRENT_INPUTS = -1;
+                                            plc.MAX_INPUTS = -1;
+                                            plc.CURRENT_OUTPUTS = -1;
+                                            plc.MAX_OUTPUTS = -1;
+                                            plc.TYPE = "Twincat2";
+                                        }
+                                        oBLC.Edit_Plc(plc);
+                                    }
+                                    reader.Close();
+                                    connection.Close();
+                                }
+                                using(SqlCommand command = new())
+                                {
+                                    command.CommandText = "SELECT TBL_ZONE.NAME, TBL_OUTLET.OUTLET_TYPE_CODE, TBL_OUTLET_DIGITAL.PLC_VARIABLE, TBL_OUTLET_DIGITAL.DIGITAL_TYPE_CODE FROM ((TBL_OUTLET INNER JOIN TBL_ZONE ON TBL_OUTLET.OUTLET_ID = TBL_ZONE.OUTLET_ID) INNER JOIN TBL_OUTLET_DIGITAL ON TBL_OUTLET.OUTLET_ID = TBL_OUTLET_DIGITAL.OUTLET_ID) WHERE ((TBL_OUTLET.OUTLET_TYPE_CODE = '001' AND (TBL_OUTLET_DIGITAL.DIGITAL_TYPE_CODE = '001' OR TBL_OUTLET_DIGITAL.DIGITAL_TYPE_CODE = '002' OR TBL_OUTLET_DIGITAL.DIGITAL_TYPE_CODE = '003')))";
+                                    command.Connection = connection;
+                                    connection.Open();
+                                    SqlDataReader reader = command.ExecuteReader();
+                                    if(reader.HasRows)
+                                    {
+                                        List<oldDB> oldDBs = new();
+                                        while(reader.Read())
+                                        {
+                                            oldDB old = new();
+                                            old.PLC_VARIABLE = "." + reader["PLC_VARIABLE"].ToString();
+                                            old.NAME = reader["NAME"].ToString();
+                                            old.DIGITAL_TYPE_CODE = reader["DIGITAL_TYPE_CODE"].ToString() switch
+                                            {
+                                                "001" => 1,
+                                                "002" => 3,
+                                                "003" => 4,
+                                                _ => throw new Exception(reader["DIGITAL_TYPE_CODE"].ToString())
+                                            };
+                                            oldDBs.Add(old);
+                                        }
+                                        for(int i = 0; i < oldDBs.Count; i++)
+                                        {
+                                            if(i != 0)
+                                            {
+                                                if(oldDBs[i].PLC_VARIABLE != oldDBs[i - 1].PLC_VARIABLE)
+                                                {
+                                                    bool isCurtain = false;
+                                                    Hardware_link hl = new();
+                                                    hl.HARDWARE_LINK_ID = -1;
+                                                    hl.PLC_ID = plc.PLC_ID;
+                                                    hl.PLC_ADDRESS = oldDBs[i].PLC_VARIABLE;
+                                                    if(oldDBs[i].DIGITAL_TYPE_CODE == 3 && oldDBs[i + 1].DIGITAL_TYPE_CODE == 3)
+                                                    {
+                                                        hl.PLC_ADDRESS += "," + oldDBs[i + 1].PLC_VARIABLE;
+                                                        isCurtain = true;
+                                                    }
+                                                    oBLC.Edit_Hardware_link(hl);
+                                                    Outlet outlet = new();
+                                                    outlet.OUTLET_ID = -1;
+                                                    outlet.CURRENT_VALUE = "0";
+                                                    outlet.ROOM_ID = room.ROOM_ID;
+                                                    outlet.NAME = oldDBs[i].NAME;
+                                                    outlet.OUTLET_TYPE_ID = oldDBs[i].DIGITAL_TYPE_CODE;
+                                                    outlet.HARDWARE_LINK_ID = hl.HARDWARE_LINK_ID;
+                                                    oBLC.Edit_Outlet(outlet);
+                                                    if(isCurtain == true)
+                                                    { i++; }
+                                                    isCurtain = false;
 
-
-                                #region Create First User
-                                User user = new();
-                                user.USER_ID = -1;
-                                user.OWNER_ID = 1;
-                                Console.WriteLine("Enter Username");
-                                user.USERNAME = Console.ReadLine();
-                                Console.WriteLine("Enter Password");
-                                user.PASSWORD = Console.ReadLine();
-                                user.ENTRY_DATE = oTools.GetDateString(DateTime.Today);
-                                user.IS_ACTIVE = true;
-                                user.USER_TYPE_CODE = "002";
-                                oBLC.Edit_User(user);
-                                #endregion
+                                                }
+                                            }
+                                        }
+                                    }
+                                    reader.Close();
+                                    connection.Close();
+                                }
+                                using(SqlCommand command = new())
+                                {
+                                    command.CommandText = "SELECT TBL_ZONE.NAME, TBL_OUTLET.OUTLET_TYPE_CODE, TBL_OUTLET_DIMMER.DIM_VARIABLE FROM ((TBL_OUTLET INNER JOIN TBL_ZONE ON TBL_OUTLET.OUTLET_ID = TBL_ZONE.OUTLET_ID) INNER JOIN TBL_OUTLET_DIMMER ON TBL_OUTLET.OUTLET_ID = TBL_OUTLET_DIMMER.OUTLET_ID) WHERE TBL_OUTLET.OUTLET_TYPE_CODE = '002'";
+                                    command.Connection = connection;
+                                    connection.Open();
+                                    SqlDataReader reader = command.ExecuteReader();
+                                    if(reader.HasRows)
+                                    {
+                                        while(reader.Read())
+                                        {
+                                            Hardware_link hl = new();
+                                            hl.HARDWARE_LINK_ID = -1;
+                                            hl.PLC_ID = plc.PLC_ID;
+                                            hl.PLC_ADDRESS = "." + reader["PLC_VARIABLE"].ToString();
+                                            oBLC.Edit_Hardware_link(hl);
+                                            Outlet outlet = new();
+                                            outlet.OUTLET_ID = -1;
+                                            outlet.CURRENT_VALUE = "0";
+                                            outlet.ROOM_ID = room.ROOM_ID;
+                                            outlet.NAME = reader["NAME"].ToString();
+                                            outlet.OUTLET_TYPE_ID = 2;
+                                            outlet.HARDWARE_LINK_ID = hl.HARDWARE_LINK_ID;
+                                            oBLC.Edit_Outlet(outlet);
+                                        }
+                                    }
+                                    reader.Close();
+                                    connection.Close();
+                                }
                             }
                             #endregion
                         }
-                            break;
+                        break;
                     case "009":
                         {
                             Params_Edit_Outlet_List params_Edit_Outlet_List = new();
